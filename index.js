1a7711fc482ab6a4d247c64709774cb251e55f95
garima <garima@webspero.com>
2023-05-19 19:55:44 +0530

    cors

--- index.js
+++ index.js
@@ -16,7 +16,15 @@ const corsOptions = {
     origin: 'https://vibras-cf75e.web.app',
 };
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'https://vibras-cf75e.web.app');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
+
 app.use(cors(corsOptions));
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
